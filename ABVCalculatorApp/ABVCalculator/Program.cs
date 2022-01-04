913cbb5b447040bb73c06e4dd1d425cc0ceeec00
micha <michaeloshea04@gmail.com>
2022-01-04 20:13:47 +1000

    Added WelcomeMessage and AskUserForOG

--- ABVCalculatorApp/ABVCalculator/Program.cs
+++ ABVCalculatorApp/ABVCalculator/Program.cs
@@ -1,4 +1,19 @@
 ﻿
+WelcomeMessage();
 
+AskUserForOG();
 
+Console.ReadLine();
 
+static void WelcomeMessage()
+{
+    Console.WriteLine("*******************************");
+    Console.WriteLine("Welcome to the ABV Calculator!!");
+    Console.WriteLine("*******************************");
+}
+
+static void AskUserForOG()
+{
+    Console.Write("Please enter your original gravity: ");
+    string userInput = Console.ReadLine();
+}
